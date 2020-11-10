dc44b5c8a8d0954fc0fbb229e3da3a7a53e9f737
Aaron Sapp <sapp.aaron@gmail.com>
2020-11-09 21:24:45 -0500

    removed legacy fallback for SwitchService

--- src/Services.ts
+++ src/Services.ts
@@ -71,8 +71,8 @@ export class SwitchService {
     accessory: PlatformAccessory,
     name: string,
     subType: string) {
-    // Check for service by subtype, then fallback to service by id only (mainly for legacy services)
-    let service = accessory.getServiceById(platform.Service.Switch, subType) || accessory.getService(platform.Service.Switch);
+    // Check for service by subtype
+    let service = accessory.getServiceById(platform.Service.Switch, subType);
     if (service === undefined) {
       service = accessory.addService(platform.Service.Switch, name, subType);
     }
